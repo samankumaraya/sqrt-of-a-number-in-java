662ad8d1140e12e4b29e46d43c0bd6b2955c2b42
Saman Kumara <94196963+samankumaraya@users.noreply.github.com>
2022-04-27 22:51:03 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,54 @@
+package RightTrianglePattern;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double number = 0, sqrt=0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        number = sc.nextDouble();
+
+        sqrt = squareRoot(number);
+
+        System.out.println("The square root of "+number+ " is " +sqrt);
+    }
+
+    private static double squareRoot(double number)
+    {
+
+        int i = 1;
+        while(true)
+        {
+
+            if(i*i == number)
+                return i;
+
+            else if(i*i > number)
+
+                return decimalSqrt(number,i-1,i);
+
+            i++;
+        }
+    }
+
+    private static double decimalSqrt(double number, double i, double j)
+    {
+
+        double midvalue = (i+j)/2;
+
+        double square = midvalue * midvalue;
+
+        if(square==number||Math.abs(square-number)<0.0000001)
+            return midvalue;
+
+        else if(square>number)
+            return decimalSqrt(number, i, midvalue);
+
+        else
+            return decimalSqrt(number, midvalue, j);
+    }
+}
